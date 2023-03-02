91eef50973a4243a13f3aa0c40edbb308725fbd4
Olegpgud <olegpgud@rambler.ru>
2023-03-02 18:32:16 +0300

    add login activity

--- app/src/main/java/com/example/test_shop/MainActivity.kt
+++ app/src/main/java/com/example/test_shop/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.test_shop
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_login = findViewById(R.id.log_in) as TextView
+// set on-click listener
+        btn_login.setOnClickListener {
+            val intent = Intent(this@MainActivity, Login::class.java)
+            startActivity(intent)
+        }
+
     }
 }
